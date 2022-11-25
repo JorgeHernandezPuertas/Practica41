aa9983454a62b35e292d017bb987708c9dd097e1
JorgeHernandezPuertas <jherpue553@g.educaand.es>
2022-11-25 09:38:48 +0100

    Hago los atributos y constructor iniciales, junto al método generarNumeroSerie

--- src/main/java/practica41/Elevador.java
+++ src/main/java/practica41/Elevador.java
@@ -4,10 +4,36 @@
  */
 package practica41;
 
+import java.time.LocalDate;
+import org.apache.commons.lang3.RandomStringUtils;
+
 /**
  *
  * @author jorge
  */
 public class Elevador {
-    
+
+    // Atributos
+    private String numeroSerie;
+    private String fabricantes;
+    private int personasMax;
+    private double pesoMax;
+    private LocalDate fechaFabricacion;
+    private LocalDate ultimaRevision;
+
+    // Constructor
+    public Elevador(String numeroSerie, String fabricantes, int personasMax, double pesoMax, LocalDate fechaFabricacion, LocalDate ultimaRevision) {
+        this.numeroSerie = generarNumeroSerie();
+        this.fabricantes = fabricantes;
+        this.personasMax = personasMax;
+        this.pesoMax = pesoMax;
+        this.fechaFabricacion = fechaFabricacion;
+        this.ultimaRevision = ultimaRevision;
+    }
+
+    // Método para generar número de serie aleatorio
+    private String generarNumeroSerie() {
+        // Genero número aleatorio de 8 digitos y lo devuelvo
+        return RandomStringUtils.randomNumeric(8); 
+    }
 }
