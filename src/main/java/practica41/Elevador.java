c701bfc02705e7afec69891904556261052d7317
JorgeHernandezPuertas <jherpue553@g.educaand.es>
2022-11-25 09:51:34 +0100

    Añado los atributos que no estaban inicialmente por los requerimientos de creación del ascensor y hago el constructor con los filtros necesarios (Incluyendo el ENUM).

--- src/main/java/practica41/Elevador.java
+++ src/main/java/practica41/Elevador.java
@@ -20,20 +20,27 @@ public class Elevador {
     private double pesoMax;
     private LocalDate fechaFabricacion;
     private LocalDate ultimaRevision;
+    private EstadoElevador estado;
+    private int plantaActual;
 
     // Constructor
-    public Elevador(String numeroSerie, String fabricantes, int personasMax, double pesoMax, LocalDate fechaFabricacion, LocalDate ultimaRevision) {
+    public Elevador(String fabricantes, int personasMax, double pesoMax) {
         this.numeroSerie = generarNumeroSerie();
         this.fabricantes = fabricantes;
-        this.personasMax = personasMax;
-        this.pesoMax = pesoMax;
-        this.fechaFabricacion = fechaFabricacion;
-        this.ultimaRevision = ultimaRevision;
+        // Implemento la condición del atributo de maximo de personas
+        this.personasMax = (personasMax < 0 || personasMax > 10) ? 0 : personasMax;
+        // Implemento la condición del atributo de peso maximo (en kgs)
+        this.pesoMax = (pesoMax < 0 || pesoMax > 800) ? 0 : pesoMax;
+        // Las fechas se crean en la fecha actual del sistema
+        this.fechaFabricacion = LocalDate.now();
+        this.ultimaRevision = LocalDate.now();
+        this.estado = null; // Su estado inicial es null
+        this.plantaActual = 0; // Se crea en la planta baja
     }
 
     // Método para generar número de serie aleatorio
     private String generarNumeroSerie() {
         // Genero número aleatorio de 8 digitos y lo devuelvo
-        return RandomStringUtils.randomNumeric(8); 
+        return RandomStringUtils.randomNumeric(8);
     }
 }
